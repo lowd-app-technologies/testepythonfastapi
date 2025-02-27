a42d7fc9df36a5932f0124b24d93d7ba8ca1cfbd
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-02-27 17:57:54 -0300

    fixing main.py again

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ def authenticate(username: str, password: str):
     options.add_argument("--headless")  
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")  
+    options.add_argument("--remote-debugging-port=9222")
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
