8274019ef72247fc0ec6191df24ff9bf2424ae99
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-02-27 08:12:44 -0300

    teste2 main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from fastapi.middleware.cors import CORSMiddleware
@@ -36,6 +37,9 @@ def authenticate(credentials: Credentials):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")  # Desabilita extensões
+    chrome_options.add_argument("--disable-plugins")  # Desabilita plugins
+    chrome_options.add_argument("--disable-translate")  # Desabilita o Google Translate
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
